4e08cf75e3424ef7aa0aa14721f38e3d36930d4d
BaekUiHeon <qordmlgjs1@gmail.com>
2023-06-02 11:35:07 +0900

    KH 객체 실습 문제
    Run 클래스에서 실행됨

---
+++ 06_ObjectOrientedProgramming/src/com/kh/practice/snack/controller/SnackController.java
@@ -0,0 +1,26 @@
+package com.kh.practice.snack.controller;
+
+import com.kh.practice.snack.model.vo.Snack;
+
+public class SnackController {
+	private Snack s= new Snack();
+
+	public SnackController() {
+		super();
+	}
+	
+	public String saveData(String kind, String name, String flavor, int numOf, int price) {
+		String result=null;
+		s.setKind(kind);
+		s.setName(name);
+		s.setFlavor(flavor);
+		s.setNumOf(numOf);
+		s.setPrice(price);
+		return result; //TODO
+	}
+	
+	public String confirmData() {
+	return s.information();
+	}
+	
+}
